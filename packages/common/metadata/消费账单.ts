e5f2848bcb2a6929992fa195e084eabf10b53bf8
songsong <2951709680@qq.com>
2025-09-28 22:56:39 +0800

    feat:添加消费账单元数据

---
+++ packages/common/metadata/消费账单.ts
@@ -0,0 +1,104 @@
+/**
+ * 消费类型
+ */
+export enum ConsumptionType {
+  娱乐会员 = '娱乐会员',
+  旅游经费 = '旅游经费',
+  买衣服 = '买衣服',
+  吃饭 = '吃饭',
+  日常开销 = '日常开销',
+}
+
+/**
+ * 消费类型相关说明
+ */
+export const ConsumptionTypeMap: Record<ConsumptionType, {
+  description: string; // 消费类型描述
+  necessary: boolean; // 是否必要消费
+  autoAdd: boolean; // 是否自动添加
+  updateLog?: {
+    updateTime: string; // 更新时间
+    updateContent: string; // 更新内容
+  }[]; // 更新日志
+}> = {
+  [ConsumptionType.娱乐会员]: {
+    necessary: false,
+    autoAdd: true,
+    description: '用于购买线上或线下的娱乐消费',
+  },
+  [ConsumptionType.旅游经费]: {
+    necessary: false,
+    autoAdd: true,
+    description: '攒旅游经费',
+  },
+  [ConsumptionType.买衣服]: {
+    necessary: true,
+    autoAdd: true,
+    description: '攒衣服经费',
+  },
+  [ConsumptionType.吃饭]: {
+    description: "吃饭消费",
+    autoAdd: false,
+    necessary: true,
+  },
+  [ConsumptionType.日常开销]: {
+    description: "水电气、油盐酱醋、生活家具、宽带话费等日常开销",
+    necessary: false,
+    autoAdd: false,
+  }
+}
+
+/**
+ * 存储地址
+ */
+export enum storageAddress {
+  支付宝_小荷包_娱乐会员 = '支付宝_小荷包_娱乐会员',
+  支付宝_小荷包_旅游经费 = '支付宝_小荷包_旅游经费',
+  支付宝_小荷包_买衣服 = '支付宝_小荷包_买衣服',
+  兴业银行_天天宝 = '兴业银行_天天宝',
+}
+
+/**
+ * 消费账单
+ */
+export const consumptionBill: {
+  createTime: string; // 创建时间
+  billDate: string;   // 账单日期
+  consumption: { // 消费明细
+    type: ConsumptionType; // 消费类型
+    price: number; // 金额
+    storage: storageAddress; // 存储地址
+  }[];
+}[] = [
+  {
+    createTime: "2025-09-28",
+    billDate: "2025-10-01",
+    consumption: [
+      {
+        type: ConsumptionType.娱乐会员,
+        price: 100,
+        storage: storageAddress.支付宝_小荷包_娱乐会员,
+      },
+      {
+        type: ConsumptionType.旅游经费,
+        price: 100,
+        storage: storageAddress.支付宝_小荷包_旅游经费,
+      },
+      {
+        type: ConsumptionType.买衣服,
+        price: 100,
+        storage: storageAddress.支付宝_小荷包_买衣服,
+      },
+      {
+        type: ConsumptionType.吃饭,
+        price: 2000,
+        storage: storageAddress.兴业银行_天天宝,
+      },
+      {
+        type: ConsumptionType.日常开销,
+        price: 400,
+        storage: storageAddress.兴业银行_天天宝,
+      },
+    ],
+  }
+]
