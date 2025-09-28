2ee8952833b2be4e4d0ce005606978571649583d
songsong <2951709680@qq.com>
2025-09-28 23:05:59 +0800

    fix:完善数据结构

--- packages/common/metadata/消费账单.ts
+++ packages/common/metadata/消费账单.ts
@@ -49,9 +49,9 @@ export const ConsumptionTypeMap: Record<ConsumptionType, {
 }
 
 /**
- * 存储地址
+ * 钱包地址
  */
-export enum storageAddress {
+export enum WalletAddress {
   支付宝_小荷包_娱乐会员 = '支付宝_小荷包_娱乐会员',
   支付宝_小荷包_旅游经费 = '支付宝_小荷包_旅游经费',
   支付宝_小荷包_买衣服 = '支付宝_小荷包_买衣服',
@@ -63,42 +63,48 @@ export enum storageAddress {
  */
 export const consumptionBill: {
   createTime: string; // 创建时间
-  billDate: string;   // 账单日期
-  consumption: { // 消费明细
+  billDate: string;   // 账单日期，通常在下月初统计上月的消费，并补充基础消费钱包
+  baseBill: { // 消费明细
     type: ConsumptionType; // 消费类型
     price: number; // 金额
-    storage: storageAddress; // 存储地址
+    wallet: WalletAddress; // 钱包地址
   }[];
+  largeBill: {
+    name: string; // 大账单名称
+    price: number; // 大账单金额
+    desc: string; // 大账单描述
+  }[]
 }[] = [
   {
     createTime: "2025-09-28",
-    billDate: "2025-10-01",
-    consumption: [
+    billDate: "2025-09-01",
+    baseBill: [
       {
         type: ConsumptionType.娱乐会员,
         price: 100,
-        storage: storageAddress.支付宝_小荷包_娱乐会员,
+        wallet: WalletAddress.支付宝_小荷包_娱乐会员,
       },
       {
         type: ConsumptionType.旅游经费,
         price: 100,
-        storage: storageAddress.支付宝_小荷包_旅游经费,
+        wallet: WalletAddress.支付宝_小荷包_旅游经费,
       },
       {
         type: ConsumptionType.买衣服,
         price: 100,
-        storage: storageAddress.支付宝_小荷包_买衣服,
+        wallet: WalletAddress.支付宝_小荷包_买衣服,
       },
       {
         type: ConsumptionType.吃饭,
         price: 2000,
-        storage: storageAddress.兴业银行_天天宝,
+        wallet: WalletAddress.兴业银行_天天宝,
       },
       {
         type: ConsumptionType.日常开销,
         price: 400,
-        storage: storageAddress.兴业银行_天天宝,
+        wallet: WalletAddress.兴业银行_天天宝,
       },
     ],
+    largeBill: [],
   }
 ]
